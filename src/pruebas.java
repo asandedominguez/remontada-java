6101d559266a83b6b67c5d878f6085448a458ab1
dam <asandedominguez@danielcastelao.org>
2026-03-26 13:34:34 +0100

    primera parte

---
+++ src/pruebas.java
@@ -0,0 +1,13 @@
+public class pruebas {
+    public static void main (String[] args) {
+        System.out.println("Hola mundo");
+        int numero = 3;
+        int n1=5555;
+        int n2=6666;
+        long n3;
+        n3 = n1*n2;
+        System.out.println(n3);
+        byte numPequeno = (byte) numero;
+        System.out.println(numPequeno);
+    }
+}
