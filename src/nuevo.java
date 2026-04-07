58d602e4406169d057ce28d6f6fbed53aa62e9ee
dam <asandedominguez@danielcastelao.org>
2026-04-07 12:58:24 +0200

    nuevo

---
+++ src/nuevo.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class nuevo {
+    public static void main (String[] args) {
+        int contador = -1;
+        while (contador != 5){
+            System.out.println(contador);
+            contador++;
+        }
+        Scanner teclado = new Scanner(System.in);
+        int opcion = 0;
+        while (opcion !=3) {
+            System.out.println("Elige una opción: ");
+            System.out.println("1. Saudar");
+            System.out.println("2. Despedir");
+            System.out.println("3. Sair");
+            opcion = teclado.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Hola");
+                    opcion = 3;
+                    break;
+                case 2:
+                    System.out.println("Adios Andres Ivan Peraza do Nascimento");
+                    opcion = 3;
+                    break;
+                case 3:
+                    System.out.println("Saliendo de René Chamorro");
+                    break;
+                default:
+                    System.out.println("Opcion no valida Isaac");
+            }
+            //for (inicicializacion;expresion condicional; incremento)
+            for (int i = 5; i < 20; i+=3) {
+                System.out.println("INDICE:"+i);
+            }
+        }
+        teclado.close();
+    }
+}
--- src/pruebas.java
+++ src/pruebas.java
@@ -2,10 +2,9 @@ public class pruebas {
     public static void main (String[] args) {
         System.out.println("Hola mundo");
         int numero = 3;
-        int n1=5555;
-        int n2=6666;
-        long n3;
-        n3 = n1*n2;
+        long n1=55555;
+        long n2=66666;
+        long n3=(n1*n2);
         System.out.println(n3);
         byte numPequeno = (byte) numero;
         System.out.println(numPequeno);
