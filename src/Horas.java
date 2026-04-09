e97a77ded624cdfc011c50bdd6ec3a7e63df747d
dam <asandedominguez@danielcastelao.org>
2026-04-09 14:24:49 +0200

    lkjhgfdsa

---
+++ src/Horas.java
@@ -0,0 +1,16 @@
+public class Horas {
+    public int horas;
+    public int minutos;
+    public int segundos;
+
+    public Horas(){
+        horas = 24;
+        minutos = 60;
+        segundos = 60;
+    }
+    public Horas(int horas, int minutos, int segundos){
+        this.horas = horas;
+        this.minutos = minutos;
+        this.segundos = segundos;
+    }
+}
