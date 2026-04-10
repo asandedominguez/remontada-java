2ecaa3da51d46c9406f6254fdb8fd25190b37da6
dam <asandedominguez@danielcastelao.org>
2026-04-10 11:25:01 +0200

    dfghjkiolp00000

--- src/Horas.java
+++ src/Horas.java
@@ -3,7 +3,7 @@ public class Horas {
     public int minutos;
     public int segundos;
 
-    public Horas(int hh, int mm, int ss) {
+    public Horas (int hh, int mm, int ss) {
         this.horas = hh;
         this.minutos = mm;
         this.segundos = ss;
@@ -19,6 +19,17 @@ public class Horas {
         double seg = (min - this.minutos) * 60;
         this.segundos = (int) seg;
     }
+    public Horas(String momento){
+        String[] partes = momento.split(":");
+        int hora = Integer.parseInt(partes[0]);
+        int mins = Integer.parseInt(partes[1]);
+        int seg = Integer.parseInt(partes[2]);
+
+        this.horas = hora;
+        this.minutos = mins;
+        this.segundos = seg;
+    }
+
 
     public int get_horas() {
         return this.horas;
@@ -49,5 +60,10 @@ public class Horas {
         System.out.println(toti.get_horas());
         System.out.println(toti.get_minutos());
         System.out.println(toti.get_segundos());
+        Horas alan = new Horas("16:45:23");
+        System.out.println(alan.get_horas());
+        System.out.println(alan.get_minutos());
+        System.out.println(alan.get_segundos());
+
     }
 }
